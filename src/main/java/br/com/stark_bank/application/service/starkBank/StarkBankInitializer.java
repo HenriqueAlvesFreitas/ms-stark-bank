dd0b3d9e034eb403090ea7b1aba9b33b9c8e080a
Henrique Freitas <haf@hotmail.com.br>
2025-12-22 13:01:10 -0300

    initializer build fix

--- src/main/java/br/com/stark_bank/application/service/starkBank/StarkBankInitializer.java
+++ src/main/java/br/com/stark_bank/application/service/starkBank/StarkBankInitializer.java
@@ -3,9 +3,11 @@ package br.com.stark_bank.application.service.starkBank;
 import br.com.stark_bank.infra.StarkBank.ProjectFactory;
 import com.starkbank.Settings;
 import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("!test")
 public class StarkBankInitializer {
 
     private final ProjectFactory projectFactory;
